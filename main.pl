92e27c5640625ccaba400024af3f5f2c0bbd5570
synth00 <63110786+synth00@users.noreply.github.com>
2024-09-19 17:05:27 +0200

    Add files via upload
    commented Code

--- main.pl
+++ main.pl
@@ -1,27 +1,29 @@
-% Fahrzeug(Position)
+% Wissensbasis
+% Fahrzeug(Position, Richtung)
 fahrzeug(nord,rechts).
 fahrzeug(ost,rechts).
 fahrzeug(sued,links).
 
 
 
-% Regel: Ein Fahrzeug kann fahren, wenn kein Fahrzeug rechts von ihm ist oder wenn es rechts abbiegt.
+% Regel: Ein Fahrzeug kann fahren, WENN
+% - kein Fahrzeug rechts von ihm ist (oder)
+% - wenn es rechts abbiegt.
 kann_fahren(Position,rechts).
 kann_fahren(Position,g_aus) :- \+ (rechts_von(Position, RechtsVonPosition), fahrzeug(RechtsVonPosition,_)).
 
-% Regel: Ein Fahrzeug kann fahren, wenn es links abbiegt und ein auto nicht von vorn kommt
+% Regel: Ein Fahrzeug kann fahren WENN 
+% - es links abbiegt und kein auto von vorn kommt (oder) 
+% - es links abbiegt und ein auto von vorn kommt, welches ebenfalls links abbiegt.
 kann_fahren(Position,links) :- \+ (rechts_von(Position, RechtsVonPosition), fahrzeug(RechtsVonPosition,_)), \+ (gegenueber(Position,Gegenrichtung), fahrzeug(Gegenrichtung,_)).
 
-% StVO §9 abs 4...linksabbieger die gegenüberstehen müssen voreinander abbiegen(modellieren als: dürfen immer fahren?!)
-
-
-% Hilfsregel: Bestimme, welche Position rechts von einer gegebenen Position liegt.
+% Hilfsregel: Bestimmung, welche Position rechts von einer gegebenen Position liegt.
 rechts_von(nord, ost). %rechts von ost ist nord
 rechts_von(ost, sued). %rechts von süd ist ost
 rechts_von(sued, west). %rechts von west ist süd
 rechts_von(west, nord). % rechts von nord ist west
 
-
+%Hilfsregel: Bestimmung welche Position Gegenüber einer jeweiligen Position liegt.
 gegenueber(ost, west).
 gegenueber(nord, sued).
 gegenueber(sued, nord).
